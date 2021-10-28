3db4ba9462fbfb36f4df8c094e72b2c4ece3f899
Sakda Suwantheerangkoon <sakda.suwantheerangkoon@krungsri.com>
2021-10-28 18:27:05 +0700

    Project Initial

---
+++ app/src/main/java/com/example/buggyweather/BuggyWeatherApp.kt
@@ -0,0 +1,19 @@
+package com.example.buggyweather
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class BuggyWeatherApp : Application() {
+
+	override fun onCreate() {
+		super.onCreate()
+
+		startKoin {
+			androidContext(this@BuggyWeatherApp)
+			androidLogger()
+			modules()
+		}
+	}
+}
