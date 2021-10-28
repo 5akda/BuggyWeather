4d48bd196a3bf37a041234678ce9c92e40e19fda
Sakda Suwantheerangkoon <sakda.suwantheerangkoon@krungsri.com>
2021-10-28 20:23:42 +0700

    Declare exception types

---
+++ app/src/main/java/com/example/buggyweather/network/exception/NotFoundException.kt
@@ -0,0 +1,3 @@
+package com.example.buggyweather.network.exception
+
+class NotFoundException(val msg: String = "data not found"): Throwable()
---
+++ app/src/main/java/com/example/buggyweather/network/exception/RemoteException.kt
@@ -0,0 +1,3 @@
+package com.example.buggyweather.network.exception
+
+class RemoteException(val msg: String = "something went wrong"): Throwable()
