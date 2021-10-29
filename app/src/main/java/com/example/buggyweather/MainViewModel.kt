fbc351132ac1faa5a029b5bba86005cc0aa7662c
Sakda Suwantheerangkoon <sakda.suwantheerangkoon@krungsri.com>
2021-10-29 17:40:07 +0700

    Add units of measurement

--- app/src/main/java/com/example/buggyweather/MainViewModel.kt
+++ app/src/main/java/com/example/buggyweather/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.buggyweather
 
 import androidx.lifecycle.ViewModel
+import com.example.buggyweather.domain.MeasuringUnits
 import com.example.buggyweather.utils.Constants
 
 class MainViewModel : ViewModel() {
 
 	var cityName: String = Constants.DEFAULT_CITY_NAME
+
+	var measuringUnits: MeasuringUnits = MeasuringUnits.METRIC
 }
