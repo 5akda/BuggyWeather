e1c259fb93d5a052e5292e3a68ca9974d9799f15
Sakda Suwantheerangkoon <sakda.suwantheerangkoon@krungsri.com>
2021-10-29 23:54:18 +0700

    Move main activity

--- app/src/main/java/com/example/buggyweather/main/MainModule.kt
+++ app/src/main/java/com/example/buggyweather/main/MainModule.kt
@@ -1,6 +1,7 @@
 package com.example.buggyweather.main
 
 import com.example.buggyweather.base.KoinModule
+import com.example.buggyweather.main.presenter.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
