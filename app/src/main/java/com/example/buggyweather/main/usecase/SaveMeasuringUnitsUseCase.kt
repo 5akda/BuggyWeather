3dd6bf7493066d62da9dccb9cebde77ce1c558a0
Sakda Suwantheerangkoon <sakda.suwantheerangkoon@krungsri.com>
2021-10-30 17:10:09 +0700

    Create units usecase

---
+++ app/src/main/java/com/example/buggyweather/main/usecase/SaveMeasuringUnitsUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.buggyweather.main.usecase
+
+import com.example.buggyweather.base.UseCase
+import com.example.buggyweather.domain.MeasuringUnits
+import com.example.buggyweather.main.repository.MeasuringUnitsDataSource
+
+class SaveMeasuringUnitsUseCase(
+		private val measuringUnitsDataSource: MeasuringUnitsDataSource
+) : UseCase<MeasuringUnits, MeasuringUnits>() {
+
+	override fun create(request: MeasuringUnits): MeasuringUnits {
+		return measuringUnitsDataSource.saveUnits(request)
+	}
+}
