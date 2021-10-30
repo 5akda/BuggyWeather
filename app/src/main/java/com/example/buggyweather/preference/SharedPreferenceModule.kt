271e6addb94e3ae1c13c6ed2797ac18a33a42286
Sakda Suwantheerangkoon <sakda.suwantheerangkoon@krungsri.com>
2021-10-30 15:37:56 +0700

    Create shared pref module

---
+++ app/src/main/java/com/example/buggyweather/preference/SharedPreferenceModule.kt
@@ -0,0 +1,21 @@
+package com.example.buggyweather.preference
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.buggyweather.base.KoinModule
+import org.koin.core.module.Module
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+
+object SharedPreferenceModule : KoinModule {
+
+	const val FEATURE_SETTING = "feature_setting"
+
+	private fun createSharedPreference(context: Context, feature: String): SharedPreferences {
+		return context.getSharedPreferences(feature, Context.MODE_PRIVATE)
+	}
+
+	override fun provide(): Module = module {
+		single(named(FEATURE_SETTING)) { createSharedPreference(get(), FEATURE_SETTING) }
+	}
+}
