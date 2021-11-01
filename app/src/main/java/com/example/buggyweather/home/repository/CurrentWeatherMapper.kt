c1f690437e1bca0a736f43528dbf750cae2366a6
Sakda Suwantheerangkoon <sakda.suwantheerangkoon@krungsri.com>
2021-11-01 12:38:26 +0700

    Refactor error handler

--- app/src/main/java/com/example/buggyweather/home/repository/CurrentWeatherMapper.kt
+++ app/src/main/java/com/example/buggyweather/home/repository/CurrentWeatherMapper.kt
@@ -1,20 +1,27 @@
 package com.example.buggyweather.home.repository
 
+import android.util.Log
 import com.example.buggyweather.domain.CurrentWeather
 import com.example.buggyweather.network.exception.NotFoundException
 import com.example.buggyweather.network.exception.RemoteException
 import retrofit2.Response
 import java.net.HttpURLConnection
 import androidx.arch.core.util.Function
+import com.example.buggyweather.network.KnownExceptionMessage
+import com.example.buggyweather.network.exception.BadRequestException
 
 class CurrentWeatherMapper: Function<Response<CurrentWeather>, CurrentWeather> {
 	override fun apply(response: Response<CurrentWeather>): CurrentWeather {
+		Log.i("ERROR ${response.code()}", response.message())
 		return when (response.code()) {
 			HttpURLConnection.HTTP_OK -> {
-				response.body() ?: throw Throwable(response.message())
+				response.body() ?: throw Throwable(KnownExceptionMessage.COMMON)
 			}
 			HttpURLConnection.HTTP_NOT_FOUND -> {
-				response.body() ?: throw NotFoundException(response.message())
+				throw NotFoundException(KnownExceptionMessage.CITY_NAME_NOT_FOUND)
+			}
+			HttpURLConnection.HTTP_BAD_REQUEST -> {
+				throw BadRequestException(KnownExceptionMessage.CITY_NAME_IS_BLANK)
 			}
 			else -> {
 				throw RemoteException(response.message())
--- app/src/main/java/com/example/buggyweather/network/exception/NotFoundException.kt
+++ app/src/main/java/com/example/buggyweather/network/exception/NotFoundException.kt
@@ -1,3 +1,3 @@
 package com.example.buggyweather.network.exception
 
-class NotFoundException(val msg: String = "data not found"): Throwable()
+class NotFoundException(msg: String): Throwable(msg)
