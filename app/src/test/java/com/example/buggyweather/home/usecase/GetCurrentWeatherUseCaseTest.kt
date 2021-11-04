e624ee2a74022d8fd47593a22167f536d4fe43d9
Sakda Suwantheerangkoon <sakda.suwantheerangkoon@krungsri.com>
2021-11-04 16:43:06 +0700

    Test current weather

---
+++ app/src/test/java/com/example/buggyweather/home/usecase/GetCurrentWeatherUseCaseTest.kt
@@ -0,0 +1,61 @@
+package com.example.buggyweather.home.usecase
+
+import com.example.buggyweather.base.UseCase
+import com.example.buggyweather.domain.CurrentWeather
+import com.example.buggyweather.domain.MeasuringUnits
+import com.example.buggyweather.helper.BaseTest
+import com.example.buggyweather.home.repository.CurrentWeatherDataSource
+import com.example.buggyweather.network.KnownExceptionMessage
+import com.example.buggyweather.network.exception.NotFoundException
+import com.example.buggyweather.network.exception.RemoteException
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.doThrow
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class GetCurrentWeatherUseCaseTest : BaseTest() {
+
+	@Mock
+	private lateinit var dataSource: CurrentWeatherDataSource
+
+	private lateinit var useCase: UseCase<Pair<String, MeasuringUnits>, CurrentWeather>
+
+	@Before
+	fun setUp() {
+		MockitoAnnotations.initMocks(this)
+		useCase = GetCurrentWeatherUseCase(dataSource)
+	}
+
+	@ExperimentalCoroutinesApi
+	@Test
+	fun getWeather_test_success() = runBlockingTest {
+		whenever(dataSource
+				.getCurrentWeather("Mountain View", MeasuringUnits.IMPERIAL.name)
+		) doReturn CurrentWeather()
+
+		val result = useCase.execute(Pair("Mountain View", MeasuringUnits.IMPERIAL))
+
+		Assert.assertEquals(CurrentWeather(), result)
+		verify(dataSource).getCurrentWeather("Mountain View", MeasuringUnits.IMPERIAL.name)
+	}
+
+	@ExperimentalCoroutinesApi
+	@Test(expected = Exception::class)
+	fun getWeather_test_Fail() = runBlockingTest {
+		val exception = NotFoundException(KnownExceptionMessage.CITY_NAME_NOT_FOUND)
+		whenever(dataSource
+				.getCurrentWeather("Mountain View", MeasuringUnits.IMPERIAL.name)
+		) doThrow exception
+
+		useCase.execute(Pair("Mountain View", MeasuringUnits.IMPERIAL))
+
+		verify(dataSource).getCurrentWeather("Mountain View", MeasuringUnits.IMPERIAL.name)
+	}
+}
